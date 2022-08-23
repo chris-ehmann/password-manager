e58b8289f57bdd79f168571e4ce3d65f6690b12b
Chris <chrisehmann@uvic.ca>
2022-08-22 22:25:44 -0700

    UI Changes

--- src/main/java/com/example/password_manager/PasswordApplication.java
+++ src/main/java/com/example/password_manager/PasswordApplication.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import org.kordamp.ikonli.javafx.FontIcon;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -16,6 +18,7 @@ public class PasswordApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(PasswordApplication.class.getResource("fxml/loginview.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Password Manager");
+        stage.getIcons().add(new Image(String.valueOf(PasswordApplication.class.getResource("img/lock-icon.png"))));
         //stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
